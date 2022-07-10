91fee20478f914e9106acaace5d902759c95aa4f
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-07-11 04:22:27 +0700

    remove PostApiController

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('/pa', \App\Http\Controllers\PostApiController::class);
