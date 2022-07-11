b3feba834346123e8208431b0c18241bb7da2ad7
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-07-11 14:03:23 +0700

    add header in show

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,6 +12,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,15 @@
 <x-guest-layout>
     <div class="container max-w-lg">
+        <div class="flex justify-between items-center mb-3 px-4 lg:px-0 py-2.5">
+            <a href="/">
+                <i class="bi bi-arrow-left"></i>
+            </a>
+            <a href="/" class="font-semibold text-2xl">{{ config('app.name') }}</a>
+            <button>
+                <i class="bi bi-upload"></i>
+            </button>
+        </div>
+
         <div class="my-3">
             <img class="object-cover" src="{{ asset('storage/images/'.$post->filename) }}"
                  alt="{{ $post->filename }}">
