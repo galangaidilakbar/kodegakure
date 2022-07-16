a3e876884617f42ba7ebefb7158f48e59511e32a
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-07-16 13:02:51 +0700

    change justify-start to justify-between

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,17 +1,20 @@
 <x-guest-layout>
     <div class="container max-w-lg">
-        <div class="flex justify-start items-center space-x-4 mb-3 px-4 lg:px-0 py-2.5">
+        <div class="flex justify-between items-center space-x-4 mb-3 px-4 lg:px-0 py-2.5">
             <a href="{{ route('index') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                 </svg>
             </a>
             <span class="font-semibold text-xl">{{ config('app.name') }}</span>
+
+            <div></div>
         </div>
 
         <div class="my-3">
-            <img class="object-cover" src="{{ asset('storage/images/'.$post->filename) }}"
-                 alt="{{ $post->filename }}">
+            <img class="object-cover" src="{{ asset('storage/images/' . $post->filename) }}"
+                alt="{{ $post->filename }}">
         </div>
 
         <div class="px-4 py-2.5 my-3 selection:bg-fuchsia-300 selection:text-fuchsia-900">
