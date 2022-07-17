991488b0d956dc4b4375086df907fe29241e02d2
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-07-17 14:27:09 +0700

    add mini profile section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,71 @@
 <x-main-layout>
-    <div id="app" class="container lg:my-1">
-        <div class="min-h-screen flex items-center justify-center">
+    <div class="container lg:my-3">
+        <div id="loading_spinner" class="min-h-screen flex items-center justify-center">
             <div class="border-8 border-gray-200 border-t-8 border-t-blue-500 rounded-full w-24 h-24 animate-spin"></div>
+        </div>
+
+        <div class="flex max-w-3xl mx-auto lg:space-x-5">
+            <div id="content" class="w-full lg:w-3/5 lg:overflow-y-scroll lg:h-screen"></div>
+
+            <div class="hidden lg:block lg:w-2/5">
+                <div class="px-4 py-3 bg-white rounded border">
+                    <div class="flex space-x-3 items-center mb-3">
+                        <img src="{{ asset('MgLSVWnD_400x400.jpeg') }}" alt="Hokage" class="w-14 h-14 rounded-full">
+                        <p class="text-base font-semibold">Galang Aidil Akbar
+                            <span class="block font-normal text-xs text-gray-600">Junior Programmer at <a href="https://phoenixkreatifdigital.com/" target="_blank" class="text-blue-600">@Phoenix</a></span>
+                        </p>
+                    </div>
+
+                    <div class="flex justify-between items-center mb-3">
+                        <span class="text-base font-semibold text-gray-500">Suggestions Projects</span>
+                        <a href="https://github.com/Galangaidil" target="_blank" class="text-sm">See all</a>
+                    </div>
+
+                    <div class="mb-3">
+                        <div class="flex justify-between items-center mb-3">
+                            <div class="flex space-x-1 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
+                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd" />
+                                </svg>
+                                <p class="text-sm font-semibold">
+                                    Siamikeu
+                                    <span class="truncate text-xs font-normal text-gray-500">Sistem Informasi keuangan</span>
+                                </p>
+                            </div>
+
+                            <a href="#" target="_blank" class="text-blue-600 text-sm">Details</a>
+                        </div>
+                        <div class="flex justify-between items-center mb-3">
+                            <div class="flex space-x-1 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd" />
+                                    <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+                                </svg>
+                                <p class="text-sm font-semibold">
+                                    Gigades.id
+                                    <span class="truncate text-xs font-normal text-gray-500">Sistem Informasi Desa Digital</span>
+                                </p>
+                            </div>
+
+                            <a href="#" target="_blank" class="text-blue-600 text-sm">Details</a>
+                        </div>
+                        <div class="flex justify-between items-center mb-3">
+                            <div class="flex space-x-1 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
+                                </svg>
+                                <p class="text-sm font-semibold">
+                                    Academica
+                                    <span class="truncate text-xs font-normal text-gray-500">Sistem Informasi Pendidikan</span>
+                                </p>
+                            </div>
+
+                            <a href="#" target="_blank" class="text-blue-600 text-sm">Details</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -36,16 +100,20 @@
                         html += `<div id="post_number_${post.slug}" class="max-w-lg mx-auto bg-white lg:border lg:rounded lg:mb-3">`+postHeader(post.slug)+postImage(post.filename)+postUtilities(post.slug)+postText(post.description, post.slug)+`</div>`
                     })
 
-                    $("#app").html(html)
+                    $("#content").html(html)
+
+                    $("#loading_spinner").remove()
                 }
 
                 function postHeader(slug) {
                     return `
                             <div class="relative">
                                 <div class="flex justify-between items-center px-4 py-3">
-                                    <div class="flex space-x-1 items-center">
-                                        <img src="${PROFILE_PICTURE_URL}" alt="Hokage" class="w-14 h-14 rounded-full">
-                                        <p class="text-sm font-semibold">Galang Aidil Akbar</p>
+                                    <div class="flex space-x-3 items-center">
+                                        <img src="${PROFILE_PICTURE_URL}" alt="Hokage" class="w-10 h-10 rounded-full ring ring-rose-500/30 ring-offset-2">
+                                        <p class="text-sm font-semibold">Galang Aidil Akbar
+                                            <span class="block font-normal text-xs text-gray-500">Konoha</span>
+                                        </p>
                                     </div>
 
                                     <button onclick="postHeaderOptions('${slug}')">
@@ -53,7 +121,7 @@
                                     </button>
                                 </div>
 
-                                <div id="kg_options_${slug}" class="hidden absolute z-10 top-14 right-4 bg-white divide-y divide-gray-100 rounded shadow-md w-44">
+                                <div id="kg_options_${slug}" class="hidden absolute z-10 top-14 right-8 bg-white divide-y divide-gray-100 rounded shadow-md w-44">
                                     <ul class="py-1 text-sm text-gray-700" aria-labelledby="dropdownInformationButton">
                                         <li>
                                             <a href="${POSTS_URL+'/'+slug+'/edit'}"
