0edf7f7a337639ef03737076e0ab3e0b2e9b2aaa
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-07-20 18:21:51 +0700

    add features markdown in show page, using marked js

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -17,8 +17,16 @@
                 alt="{{ $post->filename }}">
         </div>
 
-        <div class="px-4 py-2.5 my-3 selection:bg-fuchsia-300 selection:text-fuchsia-900">
-            <p>{{ $post->description }}</p>
+        <div class="px-4 py-2.5 my-3 selection:bg-fuchsia-300 selection:text-fuchsia-900" id="content">
+
         </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+        <script>
+            let desc = `{!! $post->description !!}`
+            document.getElementById('content').innerHTML =
+                marked.parse(desc);
+        </script>
+
     </div>
 </x-guest-layout>
