858c88148695800149128edf318882e3b7e5f8d2
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-07-22 16:27:56 +0700

    Menyamakan posisi icon

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -70,7 +70,7 @@
             })
                 .then(response => {
                     if (response.status === 201){
-                        $("#success_message").html(`${response.data.message} <span><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-green-700" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" /></svg></span>`);
+                        $("#success_message").html(`<span><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-green-700" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" /></svg></span> <div>${response.data.message}</div>`);
                         $("#title").val('');
                         $("#summary").val('');
                         $('#filename').val('');
