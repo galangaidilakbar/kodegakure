7a5478901d4ceafff0b84b3ebeb0e7fa67e8d15f
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-07-25 18:43:52 +0700

    Add kodegakure logo svg

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="Portofolio Galang Aidil Akbar | Junior Programmer di Phoenix">
+    <link rel="shortcut icon" href="{{ asset('kodegakure-logo.svg') }}" type="image/x-icon">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
